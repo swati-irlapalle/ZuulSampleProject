90156b91a3132ee7d239303f60bc30669b14e89f
swati.irlapalle <Swati@123>
2020-11-03 13:43:30 +0530

    zuul configuration

---
+++ src/test/java/com/zuul/ZuulSampleProject/ZuulSampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuul.ZuulSampleProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulSampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
